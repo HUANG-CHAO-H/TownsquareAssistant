6286968378e634540c79edfb01816fc84876e05d
黄超 <huangchao.hello@bytedance.com>
2022-10-26 02:02:36 +0800

    暂存修改（开发环境搭建中）

--- config/manifest.ts
+++ config/manifest.ts
@@ -1,22 +1,57 @@
-import path from 'path';
+// @ts-ignore
+const fs = require('fs');
+const path = require('path');
 
 const manifest: chrome.runtime.ManifestV3 = {
     manifest_version: 3,
     // 插件名称
-    name: "渲染钟楼助手",
+    name: "血染钟楼助手",
     // 插件版本
     version: "0.0.1",
     // 描述内容
     description: "向 https://www.imdodo.com/tools/clocktower/ 页面注入一些JavaScript代码，来帮助说书人完成一些自动化操作，让说书人能够更加高效的工作",
     // 后台运行的脚本
-    background: {
-        // 文件路径
-        service_worker: "background.js",
-        // 可选项，支持esm，让你可以在脚本里import其他代码
-        type: "module",
+    // background: {
+    //     // 文件路径
+    //     service_worker: "background.js",
+    //     // 可选项，支持esm，让你可以在脚本里import其他代码
+    //     type: "module",
+    // },
+
+    // （插件图标的）交互动作
+    action: {
+        // 插件图标
+        default_icon: undefined,
+        // 插件标题（hover提示文本）
+        default_title: '渲染钟楼助手',
+        // 图标弹窗（点击图标按钮后出现的弹窗）
+        default_popup: 'src/page_popup/index.html',
     },
+
     // 插件的设置，配置页面，比如说当用户在工具栏右键点击插件图标，或者导航到扩展的管理页面，并点击【详细】按钮
-    options_page: '',
+    // options_page: '',
+    // options_ui: {},
+
+    // 覆盖特定页面
+    // chrome_settings_overrides: {},
+    // chrome_ui_overrides: {},
+    // chrome_url_overrides: {},
+
+    // 向页面中注入JavaScript / CSS
+    content_scripts: [
+        {
+            // 网址匹配
+            matches: ['https://www.imdodo.com/tools/clocktower/'],
+            // 反向匹配
+            exclude_matches: undefined,
+            // 要插入的JavaScript文件
+            js: ['static/js/modulepreload-polyfill.js', 'static/js/contentScript.js'],
+            // 要插入的css文件
+            css: [],
+        }
+    ],
 }
 
-console.info(manifest);
+const manifestDir = path.resolve(__dirname, '../static/manifest.json');
+
+fs.writeFileSync(manifestDir, JSON.stringify(manifest));
--- config/vite.config.ts
+++ config/vite.config.ts
@@ -1,16 +1,51 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react'
+import {OutputBundle, OutputOptions, Plugin} from 'rollup';
+
+const rootDirName = resolve(__dirname, '../');
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        popup: resolve(__dirname, '../src/page_popup/index.html'),
-        contentScript: resolve(__dirname, '../src/content-scripts/index.html'),
-      }
+export default function ({ mode }) {
+  const isDev = mode === 'development';
+  return defineConfig({
+    plugins: [react(), ClearImportPolyfill()],
+    publicDir: resolve(rootDirName, 'static'),
+    build: {
+      target: 'esnext',
+      // 指定输出路径（相对于 项目根目录)
+      outDir: 'dist',
+      assetsDir: 'static',
+      // 指定生成静态资源的存放路径（相对于 build.outDir）。
+      rollupOptions: {
+        input: {
+          popup: resolve(rootDirName, 'src/page_popup/index.html'),
+          contentScript: resolve(rootDirName, 'src/content-scripts/index.html'),
+        },
+        output: {
+          chunkFileNames: 'static/js/[name].js',
+          entryFileNames: 'static/js/[name].js',
+          assetFileNames: 'static/[ext]/[name].[ext]',
+        }
+      },
+      // 设置为 {} 则会启用 rollup 的监听器。对于只在构建阶段或者集成流程使用的插件很常用。
+      watch: isDev ? {
+        buildDelay: 1000,
+        exclude: ['node_modules/**', 'package.json', 'yarn.lock', '.gitignore', 'tsconfig.json', 'config/**']
+      } : undefined,
+      // 设置为 false 可以禁用最小化混淆，或是用来指定使用哪种混淆器。默认为 Esbuild，它比 terser 快 20-40 倍，压缩率只差 1%-2%。
+      minify: isDev ? false : 'esbuild',
+      // 启用/禁用 gzip 压缩大小报告。压缩大型输出文件可能会很慢，因此禁用该功能可能会提高大型项目的构建性能。
+      reportCompressedSize: !isDev,
+    }
+  })
+}
+
+function ClearImportPolyfill(): Plugin {
+  return {
+    name: 'clear-import-Polyfill',
+    writeBundle(options: OutputOptions, bundle: OutputBundle) {
+      debugger;
     }
   }
-})
+}
